801e52b0291db2b8a8edfffcb66417e342fe842e
Patrick Gersaniva <patrickgersaniva@gmail.com>
2025-12-07 13:16:27 +0800

    Prepare for Render deplyoment

--- My_Portfolio/settings.py
+++ My_Portfolio/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_DIRS = [BASE_DIR / "My_Portfolio_file/static"] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
